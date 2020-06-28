8cafe731807b06370cec91d497e9c1332f4a300d
Vincent0Lee <vincent0lee95@gmail.com>
2020-06-28 11:19:43 +0800

    Outsource action tyes,  seperate reducers

--- redux_intro/src/containers/Counter/Counter.js
+++ redux_intro/src/containers/Counter/Counter.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionTypes from '../../store/actions';
 
 class Counter extends Component {
   render() {
@@ -23,7 +24,9 @@ class Counter extends Component {
           clicked={this.props.onSubtractCounter}
         />
         <hr />
-        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <button onClick={() => this.props.onStoreResult(this.props.ctr)}>
+          Store Result
+        </button>
         <ul>
           {this.props.storedResults.map((strResult) => (
             <li
@@ -41,19 +44,21 @@ class Counter extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    ctr: state.counter,
-    storedResults: state.results,
+    ctr: state.ctr.counter,
+    storedResults: state.res.results,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
-    onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
-    onAddCounter: () => dispatch({ type: 'ADD', val: 5 }),
-    onSubtractCounter: () => dispatch({ type: 'SUBTRACT', val: 5 }),
-    onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
-    onDeleteResult: (id) => dispatch({ type: 'DELETE_RESULT', resultElId: id }),
+    onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+    onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+    onAddCounter: () => dispatch({ type: actionTypes.ADD, val: 5 }),
+    onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, val: 5 }),
+    onStoreResult: (result) =>
+      dispatch({ type: actionTypes.STORE_RESULT, result }),
+    onDeleteResult: (id) =>
+      dispatch({ type: actionTypes.DELETE_RESULT, resultElId: id }),
   };
 };
 
---
+++ redux_intro/src/store/reducers/result.js
@@ -0,0 +1,32 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+  results: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        // can't using push(it will change the original array), concat return a new array
+        results: state.results.concat({ id: new Date(), value: action.result }),
+      };
+    case actionTypes.DELETE_RESULT:
+      // Another immutable array update
+      // const id = 2;
+      // const newArray = [...state.results]
+      // newArray.splice(id, 1)
+      const updatedArray = state.results.filter(
+        (result) => result.id !== action.resultElId
+      );
+      return {
+        ...state,
+        results: updatedArray,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
